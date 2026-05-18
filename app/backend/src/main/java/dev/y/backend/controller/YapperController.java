259a6c65ef34454f9e7e8e0fc9a9d28b67cd45db
Vishal Pal <vishalpal999@gmail.com>
2026-05-18 16:57:06 +0530

    Added lombok constructor annotation and updated response objects to sync partially with API contracts

--- app/backend/src/main/java/dev/y/backend/controller/YapperController.java
+++ app/backend/src/main/java/dev/y/backend/controller/YapperController.java
@@ -2,33 +2,42 @@ package dev.y.backend.controller;
 
 import dev.y.backend.dto.YapperDTO;
 import dev.y.backend.service.YapperService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1/yappers")
+@RequiredArgsConstructor
 public class YapperController {
 
     private final YapperService yapperService;
 
-    @Autowired
-    public YapperController(YapperService yapperService){
-        this.yapperService = yapperService;
-    }
-
     @GetMapping
-    public List<YapperDTO> getAllYappers(){
-        return yapperService.getAllYappers();
+    public ResponseEntity<List<YapperDTO>> getAllYappers(){
+        List<YapperDTO> yappers = yapperService.getAllYappers();
+        if(Objects.isNull(yappers)
+                || yappers.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(yappers);
     }
 
     @GetMapping("{yapperId}")
-    public YapperDTO getYapperByYapperId(@PathVariable String yapperId){
-        return yapperService.getYapperByYapperId(yapperId);
+    public ResponseEntity<YapperDTO> getYapperByYapperId(@PathVariable String yapperId){
+        YapperDTO yapperDTO = yapperService.getYapperByYapperId(yapperId);
+        if(Objects.isNull(yapperDTO)){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(yapperDTO);
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void createYapper(@RequestBody YapperDTO yapperDTO){
         yapperService.createYapper(yapperDTO);
     }
