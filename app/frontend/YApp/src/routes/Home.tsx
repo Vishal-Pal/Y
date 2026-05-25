fa908d73a9fc2904848ab519d4c61db0971639a4
Vishal Pal <vishalpal999@gmail.com>
2026-05-25 20:31:51 +0530

    Added backend fetching logic

--- app/frontend/YApp/src/routes/Home.tsx
+++ app/frontend/YApp/src/routes/Home.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import OrbitTable from "../components/OrbitTable";
-import YapperCard from "../components/YapperCard";
-import ogYapper from "../test/resources/mockData/yapper/ogYapper.json";
-import otherYappers from "../test/resources/mockData/yapper/otherYappers.json";
+import YapperCard, { type YapperCardProps } from "../components/YapperCard";
 
 export default function Home() {
+    const [yappers, setYappers] = useState<YapperCardProps[]>([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://localhost:8080/api/v1/yappers");
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const result = await response.json();
+                setYappers(result);
+            } catch (err) {
+                //setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+
+    const [ogYapper, ...otherYappers] = yappers;
+
     return (
         <>
             <h1>Welcome to YapIO</h1>
